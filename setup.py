2885f9e31115f2b0cc8066cb647def16d4c58381
Paul <paulvav@gmail.com>
2020-01-24 15:53:13 +1100

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="elasticUtils",
+    version="0.0.1",
+    author="Paul Vavich",
+    author_email="paulvav@gmail.com",
+    description="Utility Functions for Elasticsearch",
+    url="https://github.com/paulvav/elasticutils"
+)
